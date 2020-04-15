57c526f8976718cd5a2857a397bd2de6790c3142
github username <github email address>
2020-04-15 13:37:24 -0700

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Waiter
     attr_accessor :name, :yrs_experience
 
@@ -23,11 +25,48 @@ class Waiter
         end
     end
 
+    def average_tip
+        total_tip = 0
+        meals.each {|meal| total_tip += meal.tip}
+        average = total_tip / meals.length
+    end
+
     def best_tipper
         best_tipped_meal = meals.max do |meal_a, meal_b|
             meal_a.tip <=> meal_b.tip
         end
-
         best_tipped_meal.customer
     end
+
+    def worst_tipper
+        worst_tipped_meal = meals.min do |meal_a, meal_b|
+            meal_a.tip <=> meal_b.tip
+        end
+        worst_tipped_meal.customer
+    end
+
+    def customers
+        meals.map do |meal|
+            meal.customer
+        end
+    end
+
+    def frequent_customer
+        frequency = Hash.new(0)
+        customers.each { |meal| frequency[meal] += 1 }
+        frequency.sort_by {|meal, number| number}.last[0]
+    end
+
+    def self.most_experienced
+        self.all.max do |a, b|
+            a.yrs_experience <=> b.yrs_experience
+        end.average_tip
+    end
+
+    def self.least_experienced
+        self.all.min do |a, b|
+            a.yrs_experience <=> b.yrs_experience
+        end.average_tip
+    end
+
 end
